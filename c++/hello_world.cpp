c1ee8b2a22d131f47dd8eca5fb47214ec9d20d03
Daivik <114691318+User-DK@users.noreply.github.com>
2023-10-16 00:18:09 +0530

    Create hello_world.cpp

---
+++ c++/hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  string op="Hello World!!";
+  cout<<op;
+  return 0;
+}
+
+//A high-level programming language known for its versatility and efficiency, C++ is used to develop a wide range of software applications. Its syntax allows for both low-level memory manipulation and high-level abstractions, making it a popular choice for systems programming, game development, and more.
