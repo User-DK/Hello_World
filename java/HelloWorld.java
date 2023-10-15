747cb388ef69e6e065b78c2d9ea2a025ba41e02e
Omkar Bhoir <71968255+omkar787@users.noreply.github.com>
2023-10-15 18:12:06 +0530

    Added helloworld in java

---
+++ java/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+// Java is a widely-used, versatile, and platform-independent programming language known for its robustness and portability.
