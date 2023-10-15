472ac8d246f78689b6923196b1c03d5248d8cd96
Kunal Chaudhari <92564076+kc-codes@users.noreply.github.com>
2023-10-16 00:35:40 +0530

    Create hello_world.js

---
+++ JavaScript/hello_world.js
@@ -0,0 +1,5 @@
+
+console.log("Hello, World!");
+
+// JavaScript is a versatile and efficient high-level programming language, widely used for developing a broad spectrum of web/software applications. 
+// Its syntax enables both high-level abstractions and dynamic manipulation of web content, making it a popular choice for web development, interactive user interfaces, and more.
