248c47fef6d5411d873279ac66b1402cbb3039b0
Daivik <114691318+User-DK@users.noreply.github.com>
2023-10-17 21:54:25 +0530

    Rename HelloWorld.java to hello_world.java

--- java/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
-
-// Java is a widely-used, versatile, and platform-independent programming language known for its robustness and portability.
