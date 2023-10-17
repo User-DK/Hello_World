e95534994284cedc4b9970442a329c1265a68498
Daivik <114691318+User-DK@users.noreply.github.com>
2023-10-17 21:57:54 +0530

    Create hello_world.c

---
+++ c/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+/*
+Language Specific Features:
+1. Low-level, procedural language.
+2. Allows fine-grained memory manipulation.
+3. Used in operating systems and embedded systems.
+  */
