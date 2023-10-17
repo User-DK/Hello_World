af238462b91a6de88e7cef234276d91ea7714339
Daivik <114691318+User-DK@users.noreply.github.com>
2023-10-17 21:47:20 +0530

    Create hello_world.java

---
+++ java/hello_world.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+/*
+Language Specific features:
+1.Object-oriented language.
+2.Platform-independent (Write Once, Run Anywhere - WORA).
+3.Strongly typed and statically compiled.
+*/
